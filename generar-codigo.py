587fc00396ee6f36a57d3154f02d6c6a3e5dfffc
SrDeLasTinieblas <xngelo_03.h@hotmail.com>
2023-10-17 04:14:30 -0500

    ya esta

--- generar-codigo.py
+++ generar-codigo.py
@@ -1,6 +1,5 @@
 import openpyxl
 from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font, Alignment, PatternFill, Color, Border
 
 # Abre el archivo Excel
@@ -8,175 +7,63 @@ archivo_excel = load_workbook('C:\\Users\\SrDeLasTinieblas\\Downloads\\Plantilla
 
 hoja = archivo_excel.active
 
-# Inicializa listas para almacenar la información
-celdas_de_texto = []
-celdas_numericas = []
-celdas_de_estilo = []
-celdas_fusionadas = []
+# Celdas fusionadas
+celdas_fusionadas = [range_.coord.split(":") for range_ in hoja.merged_cells.ranges]
 
 codigo_csharp = ''
 
-# Recorre todas las celdas en la hoja y obtiene las celdas fusionadas
-for fila in hoja.iter_rows():
-    for celda in fila:
-        # Verifica si la celda está fusionada
-        if celda.coordinate in hoja.merged_cells:
-            celdas_fusionadas.append(celda.coordinate)
+def get_column_number(columna):
+    """
+    Convierte una letra de columna en su equivalente numérico.
+    Por ejemplo, 'A' se convierte en 1, 'B' en 2, 'Z' en 26, 'AA' en 27, 'AB' en 28, etc.
+    """
+    num = 0
+    for c in columna:
+        num = num * 26 + ord(c) - ord('A') + 1
+    return num
 
-        # Obtiene el valor de la celda
-        valor = celda.value
-        if valor:
-            # Verifica si el valor de la celda es un número o una cadena de texto
-            if isinstance(valor, (int, float)):
-                celdas_numericas.append({
-                    "fila": celda.row,
-                    "columna": celda.column,
-                    "valor": valor
-                })
-            elif isinstance(valor, str):
-                celdas_de_texto.append({
-                    "fila": celda.row,
-                    "columna": celda.column,
-                    "valor": valor
-                })
 
-        # Obtiene el estilo de la celda
-        estilo = celda.font
-        alineacion = celda.alignment
-        fondo = celda.fill
-        borde = celda.border
+for coord_inicial, coord_final in celdas_fusionadas:
+    columna_inicial, fila_inicial = openpyxl.utils.cell.coordinate_from_string(coord_inicial)
+    columna_final, fila_final = openpyxl.utils.cell.coordinate_from_string(coord_final)
 
-        estilo_celda = {
-            "fila": celda.row,
-            "columna": celda.column,
-            "fuente": {
-                "nombre": estilo.name,
-                "tamaño": estilo.size,
-                "color": estilo.color.rgb if estilo.color else None
-            },
-            "alineacion": {
-                "horizontal": alineacion.horizontal,
-                "vertical": alineacion.vertical
-            },
-            "fondo": fondo.fgColor.rgb if fondo.fgColor else None,
-            "borde": {
-                "top": borde.top.style,
-                "left": borde.left.style,
-                "right": borde.right.style,
-                "bottom": borde.bottom.style
-            }
-        }
-        celdas_de_estilo.append(estilo_celda)
-
-# Función para encontrar grupos continuos de números
-def encontrar_grupos_continuos(numeros):
-    grupos = []
-    grupo_actual = [numeros[0]]
-
-    for i in range(1, len(numeros)):
-        if numeros[i] == numeros[i - 1] + 1:
-            grupo_actual.append(numeros[i])
-        else:
-            grupos.append(grupo_actual)
-            grupo_actual = [numeros[i]]
-
-    grupos.append(grupo_actual)
-    return grupos
-
-# Inicializa un conjunto (set) para almacenar las celdas fusionadas únicas
-celdas_fusionadas_set = set()
-
-# Inicializa un diccionario para almacenar las celdas fusionadas agrupadas por letra
-celdas_fusionadas_agrupadas = {}
-
-# Recorre todas las celdas en la hoja y obtiene las celdas fusionadas
-for fila in hoja.iter_rows():
-    for celda in fila:
-        # Verifica si la celda está fusionada
-        if celda.coordinate in hoja.merged_cells:
-                
-            #print(f'Celda fusionada: {hoja.merged_cells}')
-            # Extrae la letra y número de la coordenada
-            letra, numero = celda.coordinate[0], int(celda.coordinate[1:])
-
-            # Agrega la celda fusionada al conjunto
-            celdas_fusionadas_set.add(hoja.merged_cells)
-            
-# Recorre las celdas de texto
-for celda_texto in celdas_de_texto:
-    fila = celda_texto["fila"]
-    columna = celda_texto["columna"]
-    valor = celda_texto["valor"]
-
-    # Agrega código para establecer el valor de la celda en C#
-    codigo_csharp += f'worksheet.Cells[{fila}, {columna}].Value = "{valor}";\n'
-
-# Recorre las celdas de estilo
-for estilo_celda in celdas_de_estilo:
-    fila = 11# estilo_celda["fila"]
-    columna = estilo_celda["columna"]
-    estilo = estilo_celda["fuente"]
-    alineacion = estilo_celda["alineacion"]
-    fondo = estilo_celda["fondo"]
-    borde = estilo_celda["borde"]
-
-    # Convierte el tamaño de fuente a un entero (redondea al entero más cercano)
-    tamaño_fuente = int(round(float(estilo["tamaño"])))
-
+    # Obtiene el número de la columna
+    numero_columna = get_column_number(columna_inicial)
     # Agrega código para aplicar estilos en C#
-    codigo_csharp += f'using (ExcelRange r = worksheet.Cells[{fila}, {columna}, {fila}, {columna}])\n'
+    codigo_csharp += f'using (ExcelRange r = worksheet.Cells["{columna_inicial}{fila_inicial}:{columna_final}{fila_final}"])\n'
     codigo_csharp += '{\n'
-
-    codigo_csharp += f'    r.Style.Font.SetFromFont(new Font("{estilo["nombre"]}", {tamaño_fuente}));\n'
-
-    # Agrega código para establecer el fondo
-    if fondo:
-        codigo_csharp += '    r.Style.Fill.PatternType = ExcelFillStyle.Solid;\n'
-        codigo_csharp += '    r.Style.Fill.BackgroundColor.SetColor(Color.White);\n'
-
-    # Agrega código para establecer la alineación
+    codigo_csharp += '    r.Merge = true;\n'
+    codigo_csharp += '    r.Style.Font.SetFromFont(new Font("Calibri", 11));\n'
+    codigo_csharp += '    r.Style.Font.Color.SetColor(Color.Black);\n'
     codigo_csharp += '    r.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;\n'
-    codigo_csharp += '    r.Style.VerticalAlignment = ExcelVerticalAlignment.Center;\n'
-    #codigo_csharp += f'    r.Style.HorizontalAlignment = ExcelHorizontalAlignment.{alineacion["horizontal"]};\n'
-    #codigo_csharp += f'    r.Style.VerticalAlignment = ExcelVerticalAlignment.{alineacion["vertical"]};\n'
-
-    # Agrega código para establecer los bordes
-    if borde["top"]:
-        codigo_csharp += f'    r.Style.Border.Top.Style = ExcelBorderStyle.{borde["top"].title()};\n'
-    if borde["left"]:
-        codigo_csharp += f'    r.Style.Border.Left.Style = ExcelBorderStyle.{borde["left"].title()};\n'
-    if borde["right"]:
-        codigo_csharp += f'    r.Style.Border.Right.Style = ExcelBorderStyle.{borde["right"].title()};\n'
-    if borde["bottom"]:
-        codigo_csharp += f'    r.Style.Border.Bottom.Style = ExcelBorderStyle.{borde["bottom"].title()};\n'
-
-
-
+    codigo_csharp += '    r.Style.WrapText = true;\n'
+    codigo_csharp += '    r.Style.Fill.PatternType = ExcelFillStyle.Solid;\n'
+    codigo_csharp += '    r.Style.Fill.BackgroundColor.SetColor(Color.White);\n'
+    codigo_csharp += '    r.Style.Font.Bold = false;\n'
+    codigo_csharp += '    r.Style.Border.Top.Style = ExcelBorderStyle.Thin;\n'
+    codigo_csharp += '    r.Style.Border.Left.Style = ExcelBorderStyle.Thin;\n'
+    codigo_csharp += '    r.Style.Border.Right.Style = ExcelBorderStyle.Thin;\n'
+    codigo_csharp += '    r.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;\n'
     codigo_csharp += '}\n'
-    codigo_csharp += f'worksheet.Column({columna}).AutoFit();\n\n'
-
-# Función para generar el código C# para fusionar celdas en una columna
-#(ExcelRange r = worksheet.Cells["A8:A11"])
-def generar_codigo_fusion_columna(fila_inicial, fila_final, columna):
-    return f'using (ExcelRange r = worksheet.Cells["{fila_inicial}:{fila_final}"])\n' + \
-           '{\n' + \
-           '    r.Merge = true;\n' + \
-           '}\n'
-def column_index_from_string(column_string):
-    result = 0
-    for char in column_string:
-        result = result * 26 + ord(char) - ord('A') + 1
-    return result
-
-
-# Luego, puedes recorrer el diccionario de celdas fusionadas agrupadas por letra
+    codigo_csharp += f'worksheet.Column({numero_columna}).AutoFit();\n\n'
+    
 # Recorre todas las celdas fusionadas
 for celdas_rango in hoja.merged_cells.ranges:
     coord_inicial = celdas_rango.coord.split(':')[0]
     coord_final = celdas_rango.coord.split(':')[1]
-    
-    # Agrega código para fusionar celdas en C#
-    codigo_csharp += generar_codigo_fusion_columna(coord_inicial, coord_final, coord_inicial[0])
+
+    letra_columna = coord_inicial[0]  # Obtiene la letra de la columna
+    numero_fila_inicial = int(coord_inicial[1:])  # Obtiene el número de la fila inicial
+
+    # Convierte la letra de la columna en el número correspondiente
+    numero_columna = get_column_number(letra_columna)
+
+    # Obtiene el valor de la celda fusionada
+    valor_celda = hoja[coord_inicial].value
+
+    if valor_celda:
+        # Agrega código para establecer el valor de la celda en C#
+        codigo_csharp += f'worksheet.Cells["{letra_columna}{numero_fila_inicial}"].Value = "{valor_celda}";\n'
 
 # Ruta del archivo de texto donde deseas guardar el código
 ruta_archivo = 'codigo_csharp.txt'  # Reemplaza 'ruta/del/archivo' con la ubicación deseada
@@ -184,5 +71,5 @@ ruta_archivo = 'codigo_csharp.txt'  # Reemplaza 'ruta/del/archivo' con la ubicac
 # Guarda el código C# en un archivo de texto
 with open(ruta_archivo, 'w') as archivo_txt:
     archivo_txt.write(codigo_csharp)
-    
+
 archivo_excel.close()
---
+++ probando_generar_codigo.py
@@ -0,0 +1,118 @@
+import openpyxl
+from openpyxl.styles import Font, Color, PatternFill, Border, Side, Alignment
+from openpyxl.utils import coordinate_to_tuple, get_column_letter
+
+ruta_excel = 'C:\\Users\\SrDeLasTinieblas\\Downloads\\Plantilla_Asiento.xlsx'
+workbook = openpyxl.load_workbook(ruta_excel)
+hoja = workbook.active
+
+codigo_csharp = ""  # Define la variable código C# como una cadena vacía
+
+def obtener_celdas_fusionadas(hoja):
+    try:
+        celdas_fusionadas = hoja.merged_cells.ranges
+        rangos_fusionados = [str(rango) for rango in celdas_fusionadas]
+        return rangos_fusionados
+    except Exception as e:
+        print(f"Error al procesar el archivo de Excel: {str(e)}")
+        return []
+
+def obtener_celdas_no_fusionadas(hoja, celda_inicio, celda_fin):
+    try:
+        celdas_no_fusionadas = []
+        fila_inicio, columna_inicio = coordinate_to_tuple(celda_inicio)
+        fila_fin, columna_fin = coordinate_to_tuple(celda_fin)
+        for fila in hoja.iter_rows(min_row=fila_inicio, max_row=fila_fin, min_col=columna_inicio, max_col=columna_fin):
+            for celda in fila:
+                if not any(celda.coordinate in rango for rango in hoja.merged_cells.ranges):
+                    celdas_no_fusionadas.append(celda.coordinate)
+        return celdas_no_fusionadas
+    except Exception as e:
+        print(f"Error al procesar el archivo de Excel: {str(e)}")
+        return []
+
+def generar_codigo_csharp(hoja, celdas_fusionadas, celdas_no_fusionadas):
+    codigo_csharp = ""
+    try:
+        for rango in celdas_fusionadas:
+            inicio, fin = rango.split(":")
+            estilo_primera_celda = hoja[inicio].font
+            nombre_fuente = estilo_primera_celda.name
+            tamaño_fuente = int(estilo_primera_celda.size)
+            codigo_csharp += f'using (ExcelRange r = worksheet.Cells["{rango}"])\n'
+            codigo_csharp += '{\n'
+            codigo_csharp += '    r.Merge = true;\n'
+            codigo_csharp += f'    r.Style.Font.SetFromFont("{nombre_fuente}", {tamaño_fuente});\n'
+            codigo_csharp += '    r.Style.Font.Color.SetColor(Color.Black);\n'
+            codigo_csharp += '    r.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;\n'
+            codigo_csharp += '    r.Style.WrapText = true;\n'
+            codigo_csharp += '    r.Style.Fill.PatternType = ExcelFillStyle.Solid;\n'
+            codigo_csharp += '    r.Style.Fill.BackgroundColor.SetColor(Color.White);\n'
+            codigo_csharp += '    r.Style.Font.Bold = false;\n'
+            codigo_csharp += '    r.Style.Border.Top.Style = ExcelBorderStyle.Thin;\n'
+            codigo_csharp += '    r.Style.Border.Left.Style = ExcelBorderStyle.Thin;\n'
+            codigo_csharp += '    r.Style.Border.Right.Style = ExcelBorderStyle.Thin;\n'
+            codigo_csharp += '    r.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;\n'
+            codigo_csharp += '}\n'
+            columna = get_column_number(inicio)
+            codigo_csharp += f'worksheet.Column({columna}).AutoFit();\n\n'
+        for coordenada in celdas_no_fusionadas:
+            estilo_celda = hoja[coordenada].font
+            nombre_fuente = estilo_celda.name
+            tamaño_fuente = int(estilo_celda.size)
+            codigo_csharp += f'using (ExcelRange r = worksheet.Cells["{coordenada}"])\n'
+            codigo_csharp += '{\n'
+            codigo_csharp += f'    r.Style.Font.SetFromFont("{nombre_fuente}", {tamaño_fuente});\n'
+            codigo_csharp += '    r.Style.Font.Color.SetColor(Color.Black);\n'
+            codigo_csharp += '    r.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;\n'
+            codigo_csharp += '    r.Style.WrapText = true;\n'
+            codigo_csharp += '    r.Style.Fill.PatternType = ExcelFillStyle.Solid;\n'
+            codigo_csharp += '    r.Style.Fill.BackgroundColor.SetColor(Color.White);\n'
+            codigo_csharp += '    r.Style.Font.Bold = false;\n'
+            codigo_csharp += '    r.Style.Border.Top.Style = ExcelBorderStyle.Thin;\n'
+            codigo_csharp += '    r.Style.Border.Left.Style = ExcelBorderStyle.Thin;\n'
+            codigo_csharp += '    r.Style.Border.Right.Style = ExcelBorderStyle.Thin;\n'
+            codigo_csharp += '    r.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;\n'
+            codigo_csharp += '}\n'
+            columna = get_column_number(coordenada[:-1])
+            codigo_csharp += f'worksheet.Column({columna}).AutoFit();\n'
+        return codigo_csharp
+    except Exception as e:
+        print(f"Error al procesar el archivo de Excel: {str(e)}")
+        return []
+    
+def generar_codigo_csharp_valores(hoja):
+    codigo_csharp = ""  # Define la variable código C# como una cadena vacía
+    # Recorre todas las celdas fusionadas
+    for celdas_rango in hoja.merged_cells.ranges:
+        coord_inicial = celdas_rango.coord.split(':')[0]
+
+        letra_columna = coord_inicial[0]  # Obtiene la letra de la columna
+        numero_fila_inicial = int(coord_inicial[1:])  # Obtiene el número de la fila inicial
+
+        # Obtiene el valor de la celda fusionada
+        valor_celda = hoja[coord_inicial].value
+        
+        #print(valor_celda)
+        codigo_csharp += f'worksheet.Cells["{letra_columna}{numero_fila_inicial}"].Value = "{valor_celda}";\n'
+
+    return codigo_csharp  # Devuelve el código C# generado
+
+def get_column_number(columna):
+    num = 0
+    for c in columna:
+        num = num * 26 + ord(c) - ord('A') + 1
+    return num
+
+celda_inicio = 'A11'
+celda_fin = 'L14'
+rango_celdas = f'{celda_inicio}:{celda_fin}'
+print(generar_codigo_csharp_valores(hoja))
+celdas_fusionadas = obtener_celdas_fusionadas(hoja)
+celdas_no_fusionadas = obtener_celdas_no_fusionadas(hoja, celda_inicio, celda_fin)
+
+
+
+codigo_csharp = generar_codigo_csharp(hoja, celdas_fusionadas, celdas_no_fusionadas)
+
+print(codigo_csharp)
